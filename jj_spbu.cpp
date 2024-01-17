3fade1d2f45447705c3564999f7f515eec062881
laksanautama <kussa_laksana@uhnsugriwa.ac.id>
2024-01-17 11:38:22 +0800

    submit file cpp and pdf

---
+++ jj_spbu.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+//---dilarang menambah kode di atas batas ini-----
+
+map<int, string> nama_SPBU;
+
+int SPBU_terefisien(){
+
+//CODE
+}
+
+int main(){
+
+
+return 0;
+}
